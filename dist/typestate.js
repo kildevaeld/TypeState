bb3c6f34cbb2da6dd9dfc2c2e5114dfa0ed53c5f
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-02-08 00:28:04 +0100

    added rollup

--- dist/typestate.js
+++ dist/typestate.js
@@ -1,219 +1,344 @@
-var typestate;
-(function (typestate) {
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.typestate = {})));
+}(this, (function (exports) { 'use strict';
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+/**
+    * Transition grouping to faciliate fluent api
+    */
+var Transitions =
+/*#__PURE__*/
+function () {
+  function Transitions(fsm) {
+    _classCallCheck(this, Transitions);
+
+    this.fsm = fsm;
+  }
+  /**
+   * Specify the end state(s) of a transition function
+   */
+
+
+  _createClass(Transitions, [{
+    key: "to",
+    value: function to() {
+      for (var _len = arguments.length, states = new Array(_len), _key = 0; _key < _len; _key++) {
+        states[_key] = arguments[_key];
+      }
+
+      this.toStates = states;
+      this.fsm.addTransitions(this);
+    }
     /**
-     * Transition grouping to faciliate fluent api
+     * Specify that any state in the state enum is value
+     * Takes the state enum as an argument
      */
-    var Transitions = (function () {
-        function Transitions(fsm) {
-            this.fsm = fsm;
+
+  }, {
+    key: "toAny",
+    value: function toAny(states) {
+      var toStates = [];
+
+      for (var s in states) {
+        if (states.hasOwnProperty(s)) {
+          toStates.push(states[s]);
         }
-        /**
-         * Specify the end state(s) of a transition function
-         */
-        Transitions.prototype.to = function () {
-            var states = [];
-            for (var _i = 0; _i < arguments.length; _i++) {
-                states[_i - 0] = arguments[_i];
-            }
-            this.toStates = states;
-            this.fsm.addTransitions(this);
-        };
-        /**
-         * Specify that any state in the state enum is value
-         * Takes the state enum as an argument
-         */
-        Transitions.prototype.toAny = function (states) {
-            var toStates = [];
-            for (var s in states) {
-                if (states.hasOwnProperty(s)) {
-                    toStates.push(states[s]);
-                }
-            }
-            this.toStates = toStates;
-            this.fsm.addTransitions(this);
-        };
-        return Transitions;
-    }());
-    typestate.Transitions = Transitions;
+      }
+
+      this.toStates = toStates;
+      this.fsm.addTransitions(this);
+    }
+  }]);
+
+  return Transitions;
+}();
+/**
+ * Internal representation of a transition function
+ */
+
+
+var TransitionFunction = function TransitionFunction(fsm, from, to) {
+  _classCallCheck(this, TransitionFunction);
+
+  this.fsm = fsm;
+  this.from = from;
+  this.to = to;
+};
+/**
+ * A simple finite state machine implemented in TypeScript, the templated argument is meant to be used
+ * with an enumeration.
+ */
+
+
+var FiniteStateMachine =
+/*#__PURE__*/
+function () {
+  function FiniteStateMachine(startState) {
+    var allowImplicitSelfTransition = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+
+    _classCallCheck(this, FiniteStateMachine);
+
+    this._transitionFunctions = [];
+    this._onCallbacks = {};
+    this._exitCallbacks = {};
+    this._enterCallbacks = {};
+    this._invalidTransitionCallback = void 0;
+    this.currentState = startState;
+    this._startState = startState;
+    this._allowImplicitSelfTransition = allowImplicitSelfTransition;
+  }
+
+  _createClass(FiniteStateMachine, [{
+    key: "addTransitions",
+    value: function addTransitions(fcn) {
+      var _this = this;
+
+      fcn.fromStates.forEach(function (from) {
+        fcn.toStates.forEach(function (to) {
+          // Only add the transition if the state machine is not currently able to transition.
+          if (!_this._canGo(from, to)) {
+            _this._transitionFunctions.push(new TransitionFunction(_this, from, to));
+          }
+        });
+      });
+    }
     /**
-     * Internal representation of a transition function
+     * Listen for the transition to this state and fire the associated callback
      */
-    var TransitionFunction = (function () {
-        function TransitionFunction(fsm, from, to) {
-            this.fsm = fsm;
-            this.from = from;
-            this.to = to;
-        }
-        return TransitionFunction;
-    }());
-    typestate.TransitionFunction = TransitionFunction;
+
+  }, {
+    key: "on",
+    value: function on(state, callback) {
+      var key = state.toString();
+
+      if (!this._onCallbacks[key]) {
+        this._onCallbacks[key] = [];
+      }
+
+      this._onCallbacks[key].push(callback);
+
+      return this;
+    }
     /**
-     * A simple finite state machine implemented in TypeScript, the templated argument is meant to be used
-     * with an enumeration.
+     * Listen for the transition to this state and fire the associated callback, returning
+     * false in the callback will block the transition to this state.
      */
-    var FiniteStateMachine = (function () {
-        function FiniteStateMachine(startState) {
-            this._transitionFunctions = [];
-            this._onCallbacks = {};
-            this._exitCallbacks = {};
-            this._enterCallbacks = {};
-            this._invalidTransitionCallback = null;
-            this.currentState = startState;
-            this._startState = startState;
+
+  }, {
+    key: "onEnter",
+    value: function onEnter(state, callback) {
+      var key = state.toString();
+
+      if (!this._enterCallbacks[key]) {
+        this._enterCallbacks[key] = [];
+      }
+
+      this._enterCallbacks[key].push(callback);
+
+      return this;
+    }
+    /**
+     * Listen for the transition to this state and fire the associated callback, returning
+     * false in the callback will block the transition from this state.
+     */
+
+  }, {
+    key: "onExit",
+    value: function onExit(state, callback) {
+      var key = state.toString();
+
+      if (!this._exitCallbacks[key]) {
+        this._exitCallbacks[key] = [];
+      }
+
+      this._exitCallbacks[key].push(callback);
+
+      return this;
+    }
+    /**
+     * List for an invalid transition and handle the error, returning a falsy value will throw an
+     * exception, a truthy one will swallow the exception
+     */
+
+  }, {
+    key: "onInvalidTransition",
+    value: function onInvalidTransition(callback) {
+      if (!this._invalidTransitionCallback) {
+        this._invalidTransitionCallback = callback;
+      }
+
+      return this;
+    }
+    /**
+     * Declares the start state(s) of a transition function, must be followed with a '.to(...endStates)'
+     */
+
+  }, {
+    key: "from",
+    value: function from() {
+      var _transition = new Transitions(this);
+
+      for (var _len2 = arguments.length, states = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+        states[_key2] = arguments[_key2];
+      }
+
+      _transition.fromStates = states;
+      return _transition;
+    }
+  }, {
+    key: "fromAny",
+    value: function fromAny(states) {
+      var fromStates = [];
+
+      for (var s in states) {
+        if (states.hasOwnProperty(s)) {
+          fromStates.push(states[s]);
         }
-        FiniteStateMachine.prototype.addTransitions = function (fcn) {
-            var _this = this;
-            fcn.fromStates.forEach(function (from) {
-                fcn.toStates.forEach(function (to) {
-                    // self transitions are invalid and don't add duplicates
-                    if (from !== to && !_this._validTransition(from, to)) {
-                        _this._transitionFunctions.push(new TransitionFunction(_this, from, to));
-                    }
-                });
-            });
-        };
-        /**
-         * Listen for the transition to this state and fire the associated callback
-         */
-        FiniteStateMachine.prototype.on = function (state, callback) {
-            var key = state.toString();
-            if (!this._onCallbacks[key]) {
-                this._onCallbacks[key] = [];
-            }
-            this._onCallbacks[key].push(callback);
-            return this;
-        };
-        /**
-         * Listen for the transition to this state and fire the associated callback, returning
-         * false in the callback will block the transition to this state.
-         */
-        FiniteStateMachine.prototype.onEnter = function (state, callback) {
-            var key = state.toString();
-            if (!this._enterCallbacks[key]) {
-                this._enterCallbacks[key] = [];
-            }
-            this._enterCallbacks[key].push(callback);
-            return this;
-        };
-        /**
-         * Listen for the transition to this state and fire the associated callback, returning
-         * false in the callback will block the transition from this state.
-         */
-        FiniteStateMachine.prototype.onExit = function (state, callback) {
-            var key = state.toString();
-            if (!this._exitCallbacks[key]) {
-                this._exitCallbacks[key] = [];
-            }
-            this._exitCallbacks[key].push(callback);
-            return this;
-        };
-        /**
-         * List for an invalid transition and handle the error, returning a falsy value will throw an
-         * exception, a truthy one will swallow the exception
-         */
-        FiniteStateMachine.prototype.onInvalidTransition = function (callback) {
-            if (!this._invalidTransitionCallback) {
-                this._invalidTransitionCallback = callback;
-            }
-            return this;
-        };
-        /**
-         * Declares the start state(s) of a transition function, must be followed with a '.to(...endStates)'
-         */
-        FiniteStateMachine.prototype.from = function () {
-            var states = [];
-            for (var _i = 0; _i < arguments.length; _i++) {
-                states[_i - 0] = arguments[_i];
-            }
-            var _transition = new Transitions(this);
-            _transition.fromStates = states;
-            return _transition;
-        };
-        FiniteStateMachine.prototype.fromAny = function (states) {
-            var fromStates = [];
-            for (var s in states) {
-                if (states.hasOwnProperty(s)) {
-                    fromStates.push(states[s]);
-                }
-            }
-            var _transition = new Transitions(this);
-            _transition.fromStates = fromStates;
-            return _transition;
-        };
-        FiniteStateMachine.prototype._validTransition = function (from, to) {
-            return this._transitionFunctions.some(function (tf) {
-                return (tf.from === from && tf.to === to);
-            });
-        };
-        /**
-         * Check whether a transition to a new state is valid
-         */
-        FiniteStateMachine.prototype.canGo = function (state) {
-            return this.currentState === state || this._validTransition(this.currentState, state);
-        };
-        /**
-         * Transition to another valid state
-         */
-        FiniteStateMachine.prototype.go = function (state, event) {
-            if (!this.canGo(state)) {
-                if (!this._invalidTransitionCallback || !this._invalidTransitionCallback(this.currentState, state)) {
-                    throw new Error('Error no transition function exists from state ' + this.currentState.toString() + ' to ' + state.toString());
-                }
-            }
-            else {
-                this._transitionTo(state, event);
-            }
-        };
-        /**
-         * This method is availble for overridding for the sake of extensibility.
-         * It is called in the event of a successful transition.
-         */
-        FiniteStateMachine.prototype.onTransition = function (from, to) {
-            // pass, does nothing until overidden
-        };
-        /**
-        * Reset the finite state machine back to the start state, DO NOT USE THIS AS A SHORTCUT for a transition.
-        * This is for starting the fsm from the beginning.
-        */
-        FiniteStateMachine.prototype.reset = function () {
-            this.currentState = this._startState;
-        };
-        /**
-         * Whether or not the current state equals the given state
-         */
-        FiniteStateMachine.prototype.is = function (state) {
-            return this.currentState === state;
-        };
-        FiniteStateMachine.prototype._transitionTo = function (state, event) {
-            var _this = this;
-            if (!this._exitCallbacks[this.currentState.toString()]) {
-                this._exitCallbacks[this.currentState.toString()] = [];
-            }
-            if (!this._enterCallbacks[state.toString()]) {
-                this._enterCallbacks[state.toString()] = [];
-            }
-            if (!this._onCallbacks[state.toString()]) {
-                this._onCallbacks[state.toString()] = [];
-            }
-            var canExit = this._exitCallbacks[this.currentState.toString()].reduce(function (accum, next) {
-                return accum && next.call(_this, state);
-            }, true);
-            var canEnter = this._enterCallbacks[state.toString()].reduce(function (accum, next) {
-                return accum && next.call(_this, _this.currentState, event);
-            }, true);
-            if (canExit && canEnter) {
-                var old = this.currentState;
-                this.currentState = state;
-                this._onCallbacks[this.currentState.toString()].forEach(function (fcn) {
-                    fcn.call(_this, old, event);
-                });
-                this.onTransition(old, state);
-            }
-        };
-        return FiniteStateMachine;
-    }());
-    typestate.FiniteStateMachine = FiniteStateMachine;
-})(typestate || (typestate = {}));
-// maintain backwards compatibility for people using the pascal cased version
-var TypeState = typestate;
-//# sourceMappingURL=typestate.js.map
+      }
+
+      var _transition = new Transitions(this);
+
+      _transition.fromStates = fromStates;
+      return _transition;
+    }
+  }, {
+    key: "_validTransition",
+    value: function _validTransition(from, to) {
+      return this._transitionFunctions.some(function (tf) {
+        return tf.from === from && tf.to === to;
+      });
+    }
+    /**
+     * Check whether a transition between any two states is valid.
+     *    If allowImplicitSelfTransition is true, always allow transitions from a state back to itself.
+     *     Otherwise, check if it's a valid transition.
+     */
+
+  }, {
+    key: "_canGo",
+    value: function _canGo(fromState, toState) {
+      return this._allowImplicitSelfTransition && fromState === toState || this._validTransition(fromState, toState);
+    }
+    /**
+     * Check whether a transition to a new state is valid
+     */
+
+  }, {
+    key: "canGo",
+    value: function canGo(state) {
+      return this._canGo(this.currentState, state);
+    }
+    /**
+     * Transition to another valid state
+     */
+
+  }, {
+    key: "go",
+    value: function go(state, event) {
+      if (!this.canGo(state)) {
+        if (!this._invalidTransitionCallback || !this._invalidTransitionCallback(this.currentState, state)) {
+          throw new Error('Error no transition function exists from state ' + this.currentState.toString() + ' to ' + state.toString());
+        }
+      } else {
+        this._transitionTo(state, event);
+      }
+    }
+    /**
+     * This method is availble for overridding for the sake of extensibility.
+     * It is called in the event of a successful transition.
+     */
+
+  }, {
+    key: "onTransition",
+    value: function onTransition(from, to) {} // pass, does nothing until overidden
+
+    /**
+    * Reset the finite state machine back to the start state, DO NOT USE THIS AS A SHORTCUT for a transition.
+    * This is for starting the fsm from the beginning.
+    */
+
+  }, {
+    key: "reset",
+    value: function reset() {
+      this.currentState = this._startState;
+    }
+    /**
+     * Whether or not the current state equals the given state
+     */
+
+  }, {
+    key: "is",
+    value: function is(state) {
+      return this.currentState === state;
+    }
+  }, {
+    key: "_transitionTo",
+    value: function _transitionTo(state, event) {
+      var _this2 = this;
+
+      if (!this._exitCallbacks[this.currentState.toString()]) {
+        this._exitCallbacks[this.currentState.toString()] = [];
+      }
+
+      if (!this._enterCallbacks[state.toString()]) {
+        this._enterCallbacks[state.toString()] = [];
+      }
+
+      if (!this._onCallbacks[state.toString()]) {
+        this._onCallbacks[state.toString()] = [];
+      }
+
+      var canExit = this._exitCallbacks[this.currentState.toString()].reduce(function (accum, next) {
+        return accum && next.call(_this2, state);
+      }, true);
+
+      var canEnter = this._enterCallbacks[state.toString()].reduce(function (accum, next) {
+        return accum && next.call(_this2, _this2.currentState, event);
+      }, true);
+
+      if (canExit && canEnter) {
+        var old = this.currentState;
+        this.currentState = state;
+
+        this._onCallbacks[this.currentState.toString()].forEach(function (fcn) {
+          fcn.call(_this2, old, event);
+        });
+
+        this.onTransition(old, state);
+      }
+    }
+  }]);
+
+  return FiniteStateMachine;
+}();
+
+exports.Transitions = Transitions;
+exports.TransitionFunction = TransitionFunction;
+exports.FiniteStateMachine = FiniteStateMachine;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
